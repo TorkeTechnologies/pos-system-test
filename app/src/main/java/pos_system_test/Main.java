e8bcd087d0d7d75ea728ef21e2dad441d5cf6a9d
Cedric Wellhausen <cedric.wellhausen@gmail.com>
2025-06-09 12:32:34 +0200

    Initial Commit

--- app/src/main/java/pos_system_test/Main.java
+++ app/src/main/java/pos_system_test/Main.java
@@ -1,5 +1,7 @@
 package pos_system_test;
 
+// Dies ist ein Wrapper, um das Programm aus der Test-Umgebung heraus starten zu können.
+// Um das Programm zu starten, nutzen Sie "gradle run -x test"
 public class Main {
     
     public static void main(String[] args) {
--- app/src/test/java/pos_system/DemoTestSuite.java
+++ app/src/test/java/pos_system/DemoTestSuite.java
@@ -6,25 +6,35 @@ import pos_system.database.Product;
 import pos_system.database.ProductDatabase;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeAll;
 
-class DemoTestSuite {
+// Sortieren Sie Ihre Tests sinnvoll in Klassen ein, die sich mit ähnlichen Tests beschäftigen. Denken Sie an
+// Kohäsion aus SWT. Gradle erkennt alle Klassen automatisch. Wenn Sie eine neue Klasse mit Tests erstellen,
+// müssen Sie nichts weiter machen, außer die Tests laufen zu lassen.
+class AnotherDemoTestSuite {
 
     @BeforeAll
     static void setupDatabase() {
+        // Die Produkt-Datenbank ist für einige Tests wichtig. Setzen Sie entweder diese über init() oder, wenn
+        // gefordert, mocken Sie die Datenbank.
         ProductDatabase.init();
     }
 
+    // Dies ist ein Test. Erinnern Sie sich daran, wie wir Tests in der Vorlesung und Übung definiert haben.
+    // Auch hier gilt das Single-Responsibility-Principle. Ein JUnit @Test sollte genau einen Tests
+    // "auf Papier" abdecken und nicht mehr.
     @Test
-    void succeedingExampleTest() {
+    void succeedingExampleTest2() {
+        // Machen Sie sich mit der API der Software in der JavaDoc vertraut.
         Product product = ProductDatabase.getProductById(0);
+
+        // Sie können assert oder die JUnit assert-Familie (assertEquals, ...) verwenden.
         assert product.getId() == 0;
     }
 
     @Test
-    void failingExampleTest() {
+    void failingExampleTest2() {
         Product product = ProductDatabase.getProductById(1);
-        assert product.getName().equals("Reis");
+        assertEquals(product.getName(), "Reis");
     }
 }
